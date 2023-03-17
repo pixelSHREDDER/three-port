2413872aa736c0fb6d6020483d10e7b3f4f2b12d
Mike DeVine <pixelshredder@gmail.com>
2023-03-17 14:35:12 -0700

    Install, typescript setup, and some tinkering

---
+++ app/src/pages/500.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Error() {
+  return <h1>500 - Something went wrong</h1>
+}
--- my-app/README.md
+++
@@ -1,3 +0,0 @@
-# my-app
-
-This project was bootstrapped with [`create-r3f-app`](https://github.com/utsuboco/create-r3f-app)
